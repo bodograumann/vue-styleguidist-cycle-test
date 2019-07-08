f6547dc24e0582fe00da20f013160d786dedf83a
Barthelemy Ledoux <bart.ledoux@enablon.com>
2019-07-08 15:27:19 -0500

    async registration of component

--- src/components/Component3/Component3.vue
+++ src/components/Component3/Component3.vue
@@ -3,11 +3,9 @@
 </template>
 
 <script>
-import Component2 from "../Component2/Component2.vue";
-
 export default {
   components: {
-    Component2
+    Component2: () => import("../Component2/Component2.vue")
   }
 };
 </script>
